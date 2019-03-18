b63f1c6fb8150a82a91519c90413412bfdbb8df2
sidneyrens <sidneyrens@gmail.com>
2019-03-18 15:31:53 -0500

    restarted project. have added the model.

---
+++ playerscore/src/main/kotlin/com/rens/playerscore/PlayerscoreApplication.kt
@@ -0,0 +1,11 @@
+package com.rens.playerscore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlayerscoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlayerscoreApplication>(*args)
+}
---
+++ playerscore/src/main/kotlin/com/rens/playerscore/repository/PlayerRepository.kt
@@ -0,0 +1,2 @@
+package com.rens.playerscore.repository
+
